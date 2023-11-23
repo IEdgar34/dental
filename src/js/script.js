736cb1b7d07f494579a1ff5d5cb4bb0f831fd772
Edgar levonyan <edo.jan.94@bk.ru>
2023-11-23 20:54:43 +0300

    commit

---
+++ src/js/script.js
@@ -0,0 +1,60 @@
+$(document).ready(function(){
+    $('.reviews__slide').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        autoplay: true,
+        autoplaySpeed: 2000,
+        slidesToShow: 3,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>'
+    });
+
+
+    function valideForms(form){
+        $(form).validate({
+            rules: {
+              name: "required",
+              phone: "required",
+              
+            },
+            messages: {
+              name: "пожалуйста введите ваше имя ",
+              phone: "введите ваш номер телефона",
+              
+            }
+        });
+    };
+
+    valideForms('#consultation form')
+
+
+
+    $("#phone").mask("+7(999) 999-99-99");
+
+
+
+    $('form').submit(function(e){
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "../mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function(){
+            $('form').trigger('reset');
+
+           
+            
+        });
+       return false;
+   });
+
+
+   new WOW().init();
+    
+
+  
+   
+   
+   
+
+});
